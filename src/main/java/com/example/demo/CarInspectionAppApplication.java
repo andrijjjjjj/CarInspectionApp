0683acb01eea080dc453c5e1e209e1fd1e6ca59c
apokhva <apokhvalit@gmail.com>
2020-11-29 22:00:06 -0600

    Apache Tomcat modifications. Using WAR.

--- src/main/java/com/example/demo/CarInspectionAppApplication.java
+++ src/main/java/com/example/demo/CarInspectionAppApplication.java
@@ -9,12 +9,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CarInspectionAppApplication {
+public class CarInspectionAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarInspectionAppApplication.class, args);
 	}
+	
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(CarInspectionAppApplication.class);
+	  }
 	
 }
